0ad97979880f5842df6ddf72182e47470e74618b
annamaulina654 <143859907+annamaulina654@users.noreply.github.com>
2023-09-02 10:59:38 +0700

    Add files via upload
    uploud file

---
+++ latihan.py
@@ -0,0 +1,27 @@
+nama_saya = str(input("masukkan nama kamu = "))
+print(nama_saya, "bertipe", type(nama_saya))
+
+umur = int(input("masukkan umur kamu = "))
+print (umur, "bertipe", type(umur))
+
+IPK = float(input("masukkan IPK kamu = "))
+print (IPK, "bertipe", type(IPK))
+
+menikah = (input("masukkan IPK kamu = "))
+print ("menikah?",menikah, "bertipe", type(menikah))
+
+matkul_diambil = ["alpro", "sig"]
+print("Matkul : ",matkul_diambil, "bertipe", type(matkul_diambil))
+
+matkul_diambil2 = ("web", "permvis")
+print("Matkul Lain : ",matkul_diambil2, "bertipe", type(matkul_diambil2))
+
+matkul_diambil3 = {"Alpro", "ALpro", "Pemweb", "Data Science", "Data Science"}
+print("Matkul Lain-Lain : ",matkul_diambil3, "bertipe", type(matkul_diambil3))
+
+print ()
+print()
+
+print(20 / 3)
+print(20 // 3)
+print(20 % 3)
